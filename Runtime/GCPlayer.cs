571148167c1d095158d71fe69ed1bb3380fb1418
Antti Luukka <antti.luukka@gmail.com>
2024-06-07 14:34:24 +0300

    API documentation for GCPlayer.cs

--- Runtime/GCPlayer.cs
+++ Runtime/GCPlayer.cs
@@ -6,26 +6,47 @@ namespace DSB.GC
 {
     public class GCPlayer : MonoBehaviour, IGCPlayer
     {
+        private int playerId = -1;
+        private string playerName;
         private Color color;
-        private int playerId;
 
+        /// <summary>
+        /// Do not call this in your game script. This is called by the GamingCouch script.
+        /// If you want to do something custom with the options on player setup, override this method.
+        /// NOTE: Don't forget to call base.GamingCouchSetup(options) when overriding.
+        /// </summary>
+        /// <param name="options">Options provided by the platform </param>
         public virtual void GamingCouchSetup(GCPlayerSetupOptions options)
         {
             playerId = options.playerId;
-            name = options.name;
+            playerName = options.name;
             color = options.color;
         }
 
+        /// <summary>
+        /// Get the player id provided by GamingCouch in GCPlayer.GamingCouchSetup.
+        /// </summary>
         public virtual int GetId()
         {
+            if (playerId == -1)
+            {
+                throw new System.Exception("Player id is not set. Wait for GamingCouchSetup to be called first. If you are overriding GamingCouchSetup, make sure to call base.GamingCouchSetup(options) in your override.");
+            }
+
             return playerId;
         }
 
+        /// <summary>
+        /// Get the player name provided by GamingCouch in GCPlayer.GamingCouchSetup.
+        /// </summary>
         public virtual string GetName()
         {
-            return name;
+            return playerName;
         }
 
+        /// <summary>
+        /// Get the player color provided by GamingCouch in GCPlayer.GamingCouchSetup.
+        /// </summary>
         public virtual Color GetColor()
         {
             return color;
