353024e647b4ab77897f76635eab8134d97ea4fa
Antti Luukka <antti.luukka@gmail.com>
2025-03-14 01:50:29 +0200

    Prototype observer

--- Runtime/GCSetupOptions.cs
+++ Runtime/GCSetupOptions.cs
@@ -5,6 +5,7 @@ namespace DSB.GC
     [System.Serializable]
     public class GCSetupOptions
     {
+        public bool isHost;
         public string gameModeId;
 
         public static GCSetupOptions CreateFromJSON(string optionsJson)
