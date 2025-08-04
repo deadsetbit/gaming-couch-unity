bd08f34ee3ab498695c0ffa860c8c642028e3e72
Antti Luukka <antti.luukka@gmail.com>
2025-08-04 14:04:12 +0300

    Enable exceptions on release builds, as disabling them appears to cause unrecoverable crashes

--- Editor/Utils/WebBuildOptimizer.cs
+++ Editor/Utils/WebBuildOptimizer.cs
@@ -3,85 +3,87 @@ using UnityEditor.Build;
 
 public class WebBuildOptimizer
 {
-    [MenuItem("GamingCouch/WebGL Build/Apply release build settings (slow build)")]
-    public static void OptimizeBuild()
-    {
-        var namedBuildTarget = NamedBuildTarget.WebGL;
+        [MenuItem("GamingCouch/WebGL Build/Apply release build settings (slow build)")]
+        public static void ApplyReleaseBuildSettings()
+        {
+                var namedBuildTarget = NamedBuildTarget.WebGL;
 
-        // Set IL2CPP code generation to Optimize Size 
-        PlayerSettings.SetIl2CppCodeGeneration(namedBuildTarget,
-                                        Il2CppCodeGeneration.OptimizeSize);
+                // Set IL2CPP code generation to Optimize Size 
+                PlayerSettings.SetIl2CppCodeGeneration(namedBuildTarget,
+                                                Il2CppCodeGeneration.OptimizeSize);
 
-        // Set the Managed Stripping Level to High
-        PlayerSettings.SetManagedStrippingLevel(namedBuildTarget,
-                                            ManagedStrippingLevel.High);
+                // Set the Managed Stripping Level to High
+                PlayerSettings.SetManagedStrippingLevel(namedBuildTarget,
+                                                    ManagedStrippingLevel.High);
 
-        // Strip unused mesh components           
-        PlayerSettings.stripUnusedMeshComponents = true;
+                // Strip unused mesh components           
+                PlayerSettings.stripUnusedMeshComponents = true;
 
-        // Enable data caching
-        PlayerSettings.WebGL.dataCaching = true;
+                // Enable data caching
+                PlayerSettings.WebGL.dataCaching = true;
 
-        // Set the compression format to Brotli
-        // PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Brotli;
+                // Set the compression format to Brotli
+                // disabled as the server will serve the files compressed by default
+                // PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Brotli;
 
-        // Deactivate exceptions
-        PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.None;
+                // Minimal exception support
+                // deactivating is not smart as it will cause the game to crash if an error occurs
+                PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.ExplicitlyThrownExceptionsOnly;
 
-        // Deactivate debug symbols
-        PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Off;
-
-#if UNITY_2023_1_OR_NEWER
-        //Enable WebAssembly 2023 features
-        PlayerSettings.WebGL.wasm2023 = true;
-#endif
-
-        // Enable development build
-        EditorUserBuildSettings.development = false;
-
-        // Set Platform Settings to optimize for disk size (LTO)
-        UnityEditor.WebGL.UserBuildSettings.codeOptimization = UnityEditor.WebGL.WasmCodeOptimization.DiskSizeLTO;
-    }
-
-
-    [MenuItem("GamingCouch/WebGL Build/Apply dev build settings (fast build)")]
-    public static void SpeedupBuild()
-    {
-        var namedBuildTarget = NamedBuildTarget.WebGL;
-
-        // Set IL2CPP code generation to Optimize Size 
-        PlayerSettings.SetIl2CppCodeGeneration(namedBuildTarget,
-                                        Il2CppCodeGeneration.OptimizeSpeed);
-
-        // Set the Managed Stripping Level to High
-        PlayerSettings.SetManagedStrippingLevel(namedBuildTarget,
-                                            ManagedStrippingLevel.Disabled);
-
-        // Strip unused mesh components           
-        PlayerSettings.stripUnusedMeshComponents = false;
-
-        // Enable data caching
-        PlayerSettings.WebGL.dataCaching = false;
-
-        // Set the compression format to Brotli
-        PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
-
-        // Deactivate exceptions
-        PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.FullWithStacktrace;
-
-        // Deactivate debug symbols
-        PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Embedded;
+                // Deactivate debug symbols
+                PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Off;
 
 #if UNITY_2023_1_OR_NEWER
         //Enable WebAssembly 2023 features
         PlayerSettings.WebGL.wasm2023 = true;
 #endif
 
-        // Enable development build
-        EditorUserBuildSettings.development = true;
+                // Enable development build
+                EditorUserBuildSettings.development = false;
 
-        // Set Platform Settings to optimize for disk size (LTO)
-        UnityEditor.WebGL.UserBuildSettings.codeOptimization = UnityEditor.WebGL.WasmCodeOptimization.BuildTimes;
-    }
+                // Set Platform Settings to optimize for disk size (LTO)
+                UnityEditor.WebGL.UserBuildSettings.codeOptimization = UnityEditor.WebGL.WasmCodeOptimization.DiskSizeLTO;
+        }
+
+
+        [MenuItem("GamingCouch/WebGL Build/Apply dev build settings (fast build)")]
+        public static void ApplyDevBuildSettings()
+        {
+                var namedBuildTarget = NamedBuildTarget.WebGL;
+
+                // Set IL2CPP code generation to Optimize Size 
+                PlayerSettings.SetIl2CppCodeGeneration(namedBuildTarget,
+                                                Il2CppCodeGeneration.OptimizeSpeed);
+
+                // Set the Managed Stripping Level to High
+                PlayerSettings.SetManagedStrippingLevel(namedBuildTarget,
+                                                    ManagedStrippingLevel.Disabled);
+
+                // Strip unused mesh components           
+                PlayerSettings.stripUnusedMeshComponents = false;
+
+                // Enable data caching
+                PlayerSettings.WebGL.dataCaching = false;
+
+                // Set the compression format to Brotli
+                PlayerSettings.WebGL.compressionFormat = WebGLCompressionFormat.Disabled;
+
+                // Deactivate exceptions
+                PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.FullWithStacktrace;
+
+                // Deactivate debug symbols
+                PlayerSettings.WebGL.debugSymbolMode = WebGLDebugSymbolMode.Embedded;
+
+#if UNITY_2023_1_OR_NEWER
+        //Enable WebAssembly 2023 features
+        PlayerSettings.WebGL.wasm2023 = true;
+#endif
+
+                // Enable development build
+                EditorUserBuildSettings.development = true;
+
+                // Set Platform Settings to optimize for disk size (LTO)
+                UnityEditor.WebGL.UserBuildSettings.codeOptimization = UnityEditor.WebGL.WasmCodeOptimization.BuildTimes;
+        }
 
 }
